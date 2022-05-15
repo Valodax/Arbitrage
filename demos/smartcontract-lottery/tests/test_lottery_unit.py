f5f3fa1b8a45d792ec42149f5eeb4224a314bb18
Valodax <48880504+Valodax@users.noreply.github.com>
2022-05-16 01:31:41 +0200

    smart contract lottery

---
+++ demos/smartcontract-lottery/tests/test_lottery_unit.py
@@ -0,0 +1,26 @@
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from brownie import Lottery, accounts, config, network
+from scripts.deploy_lottery import deploy_lottery
+from web3 import Web3
+import pytest
+
+
+def test_get_entrance_fee():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    # ARRANGE
+    lottery = deploy_lottery()
+    # ACT
+    # assuming 2000eth/usd
+    # 2000/1 == 50/x == 0.025
+    expected_entrance_fee = Web3.toWei(0.025, "ether")
+    entrance_fee = lottery.getEntranceFee()
+    # ASSERT
+    assert expected_entrance_fee == entrance_fee
+
+def test_cant_enter_unless_started():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    pytest.skip()
+    lottery = deploy_lottery()
+    with pytest.raises(exceptions.VirtualMachineError):
+        lottery.enter({"from":get_account(),})
