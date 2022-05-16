aa2ec6a7f865fd9bd31b5b579e859d387494e8f0
Valodax <48880504+Valodax@users.noreply.github.com>
2022-05-16 17:25:24 +0200

    ERC-20 Token

---
+++ demos/aave_brownie_py/README.md
@@ -0,0 +1,1 @@
+Deposit some ETH into AAVE
---
+++ demos/erc20-brownie/scripts/deploy.py
@@ -0,0 +1,13 @@
+from brownie import accounts, config, OurToken, network
+from scripts.helpful_scripts import get_account
+from web3 import Web3
+
+inital_supply = Web3.toWei(1000, "ether")
+
+def deploy_our_token(supply):
+    account = get_account()
+    our_token = OurToken.deploy(supply,{"from": account}, publish_source=True)
+    print(our_token.name())
+
+def main():
+    deploy_our_token(inital_supply)
