ef66c6c06ccc38bbbb525d7b2a71c52a5681547b
Valodax <48880504+Valodax@users.noreply.github.com>
2022-05-16 18:40:19 +0200

    update

--- demos/aave_brownie_py/scripts/aave_borrow.py
+++ demos/aave_brownie_py/scripts/aave_borrow.py
@@ -1,6 +1,10 @@
 from brownie import network, config, interface
 from scripts.helpful_scripts import get_account
 from scripts.get_weth import get_weth
+from web3 import Web3
+
+# 0.1
+amount = Web3.toWei(0.1, "ether")
 
 
 def main():
@@ -11,7 +15,47 @@ def main():
     # abi
     # address
     lending_pool = get_lending_pool()
-    print(lending_pool)
+    # approve sending ERC20 tokens
+    approve_erc20(amount, lending_pool.address, erc20_address, account)
+    print("Depositing...")
+    tx = lending_pool.deposit(
+        erc20_address, amount, account.address, 0, {"from": account}
+    )
+    tx.wait(1)
+    print("Deposited!")
+    borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+    print("Let's borrow!")
+    # DAI in terms of ETH
+    dai_eth_price = get_asset_price(dai_eth_price_feed)
+
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_threshold,
+        ltv,
+        health_factor,
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+    print(f"You have {total_collateral_eth} worth of ETH deposited")
+    print(f"You can borrow {available_borrow_eth} worth of ETH")
+    print(f"You have {total_debt_eth} worth of ETH borrowed")
+    return (float(available_borrow_eth), float(total_debt_eth))
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    # ABI
+    # ADDRESS
+    print("Approving ERC20 token...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved!")
+    return tx
 
 
 def get_lending_pool():
