33793465fb7808b14d051acedd85bfa7b64ecdde
Valodax <48880504+Valodax@users.noreply.github.com>
2022-05-16 18:14:27 +0200

    aave code

--- demos/aave_brownie_py/scripts/get_weth.py
+++ demos/aave_brownie_py/scripts/get_weth.py
@@ -1,3 +1,7 @@
+from scripts.helpful_scripts import get_account
+from brownie import interface, config, network
+
+
 def main():
     get_weth()
 
@@ -6,5 +10,8 @@ def get_weth():
     """
     Mints WETH by depositing ETH.
     """
-    # we need an abi and an address as per usual.
-    get_account()
+    account = get_account()
+    weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
+    tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18})
+    tx.wait(1)
+    print("Received 0.1 WETH")
