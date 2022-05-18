47bf0332461cf0c51b65108b105b6081d4d60e4a
Valodax <48880504+Valodax@users.noreply.github.com>
2022-05-18 17:27:18 +0200

    scripts

---
+++ upgrades/scripts/01_deploy_box.py
@@ -0,0 +1,23 @@
+from scripts.helpful_scripts import get_account, encode_function_data
+from brownie import network, Box, ProxyAdmin, TransparentUpgradeableProxy, Contract
+
+
+def main():
+    account = get_account()
+    print(f"Deploying to {network.show_active()}")
+    box = Box.deploy({"from": account})
+
+    proxy_admin = ProxyAdmin.deploy({"from": account})
+
+    # initializer = box.store, 1
+    box_encoded_initializer_function = encode_function_data()
+
+    proxy = TransparentUpgradeableProxy.deploy(
+        box.address,
+        proxy_admin.address,
+        box_encoded_initializer_function,
+        {"from": account, "gas_limit": 1000000},
+    )
+    print(f"Proxy Deployed to {proxy}, you can now upgrade to V2!")
+    proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
+    print(proxy_box.retrieve())
