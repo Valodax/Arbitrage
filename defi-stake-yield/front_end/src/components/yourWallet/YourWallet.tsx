ee9372a7dfae3e3768aaefdbe400b27262ce7a92
Valodax <48880504+Valodax@users.noreply.github.com>
2022-05-20 16:17:09 +0200

    wallet balance

--- defi-stake-yield/front_end/src/components/yourWallet/YourWallet.tsx
+++ defi-stake-yield/front_end/src/components/yourWallet/YourWallet.tsx
@@ -2,17 +2,44 @@ import { Token } from "../Main"
 import React, { useState } from "react"
 import { Box } from "@material-ui/core"
 import { TabContext, TabList, TabPanel } from "@material-ui/lab"
+import { Tab } from "@material-ui/core"
+import { WalletBalance } from "./WalletBalance"
+
 
 interface YourWalletProps {
     supportedTokens: Array<Token>
 }
 export const YourWallet = ({ supportedTokens }: YourWalletProps) => {
     const [selectedTokenIndex, setSelectedTokenIndex] = useState<number>(0)
+    //changes selected token
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+        setSelectedTokenIndex(parseInt(newValue))
+    }
+
     return (
         <Box>
             <h1>Your Wallet!</h1>
             <Box>
                 <TabContext value={selectedTokenIndex.toString()}>
+                    <TabList onChange={handleChange} aria-label="stake form tabs">
+                        {supportedTokens.map((token, index) => {
+                            return (
+                                <Tab label={token.name}
+                                    value={index.toString()}
+                                    key={index} />
+                            )
+                        })}
+                    </TabList>
+                    {supportedTokens.map((token, index) => {
+                        return (
+                            <TabPanel value={index.toString()} key={index}>
+                                <div>
+                                    <WalletBalance token={supportedTokens[selectedTokenIndex]} />
+                                    2. Stake Button
+                                </div>
+                            </TabPanel>
+                        )
+                    })}
                 </TabContext>
             </Box>
         </Box>
