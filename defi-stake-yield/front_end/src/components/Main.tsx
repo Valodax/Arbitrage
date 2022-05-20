97d1836531cf48cf2404fccf456392c71ce42a06
Valodax <48880504+Valodax@users.noreply.github.com>
2022-05-20 15:38:19 +0200

    front_end_update

---
+++ defi-stake-yield/front_end/src/components/Main.tsx
@@ -0,0 +1,51 @@
+import { useEthers } from "@usedapp/core"
+import helperConfig from "../helper-config.json"
+import networkMapping from "../chain-info/deployments/map.json"
+import { constants } from "ethers"
+import brownieConfig from "../brownie-config.json"
+import dapp from "../dapp.png"
+import eth from "../eth.png"
+import dai from "../dai.png"
+import { YourWallet } from "./yourWallet"
+
+export type Token = {
+    image: string
+    address: string
+    name: string
+}
+
+export const Main = () => {
+    // show token values from the wallet
+    // get the address of different tokens
+    // get the  balance of the users wallet
+
+    // send the brownie-config to our `src` folder
+    // send the build folder
+    const { chainId } = useEthers()
+    const networkName = chainId ? helperConfig[chainId] : "dev"
+
+    const dappTokenAddress = chainId ? networkMapping[String(chainId)]['DappToken'][0] : constants.AddressZero
+    const wethTokenAddress = chainId ? brownieConfig["networks"][networkName]["weth_token"] : constants.AddressZero
+    const fauTokenAddress = chainId ? brownieConfig["networks"][networkName]["fau_token"] : constants.AddressZero
+
+    const supportedTokens: Array<Token> = [
+        {
+            image: dapp,
+            address: dappTokenAddress,
+            name: "DAPP"
+        },
+        {
+            image: eth,
+            address: wethTokenAddress,
+            name: "WETH"
+        },
+        {
+            image: dai,
+            address: fauTokenAddress,
+            name: "DAI"
+        }
+    ]
+
+    //const dappTokenAddress ??
+    return (<YourWallet supportedTokens={supportedTokens} />)
+}
