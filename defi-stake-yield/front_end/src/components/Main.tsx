75dbc4a402b2bad5e19c1eea255b6f1d2dc43c31
Valodax <48880504+Valodax@users.noreply.github.com>
2022-05-20 16:17:14 +0200

    wallet

--- defi-stake-yield/front_end/src/components/Main.tsx
+++ defi-stake-yield/front_end/src/components/Main.tsx
@@ -7,6 +7,7 @@ import dapp from "../dapp.png"
 import eth from "../eth.png"
 import dai from "../dai.png"
 import { YourWallet } from "./yourWallet"
+import { makeStyles } from "@material-ui/core"
 
 export type Token = {
     image: string
@@ -14,18 +15,27 @@ export type Token = {
     name: string
 }
 
+const useStyles = makeStyles((theme) => ({
+    title: {
+        color: theme.palette.common.white,
+        textAlign: "center",
+        padding: theme.spacing(4)
+    }
+}))
+
 export const Main = () => {
-    // show token values from the wallet
-    // get the address of different tokens
-    // get the  balance of the users wallet
+    // Show token values from the wallet
+    // Get the address of different tokens
+    // Get the balance of the users wallet
 
     // send the brownie-config to our `src` folder
     // send the build folder
-    const { chainId } = useEthers()
+    const classes = useStyles()
+    const { chainId, error } = useEthers()
     const networkName = chainId ? helperConfig[chainId] : "dev"
-
-    const dappTokenAddress = chainId ? networkMapping[String(chainId)]['DappToken'][0] : constants.AddressZero
-    const wethTokenAddress = chainId ? brownieConfig["networks"][networkName]["weth_token"] : constants.AddressZero
+    let stringChainId = String(chainId)
+    const dappTokenAddress = chainId ? networkMapping[stringChainId]["DappToken"][0] : constants.AddressZero
+    const wethTokenAddress = chainId ? brownieConfig["networks"][networkName]["weth_token"] : constants.AddressZero // brownie config
     const fauTokenAddress = chainId ? brownieConfig["networks"][networkName]["fau_token"] : constants.AddressZero
 
     const supportedTokens: Array<Token> = [
